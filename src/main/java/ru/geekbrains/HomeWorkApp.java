d8bc7847d1dad5bfdcd9aff388fd76b5854cbb11
nolty <nolty81@gmail.com>
2022-04-08 18:14:50 +0500

    Практическое задание к уроку №7

--- src/main/java/ru/geekbrains/HomeWorkApp.java
+++ src/main/java/ru/geekbrains/HomeWorkApp.java
@@ -2,6 +2,7 @@ package ru.geekbrains;
 
 public class HomeWorkApp {
     public static void main(String[] args) {
-
+        TestRunner.start(TestClass.class);
+        // TestRunner.start("ru.geekbrains.TestClass");
     }
 }
